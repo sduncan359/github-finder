2715992dec5e532815ff81061e9d1d88a086d87c
Steve Duncan <67704665+sduncan-propack@users.noreply.github.com>
2023-01-22 17:26:00 -0800

    finish repos section

--- src/context/github/GithubReducer.js
+++ src/context/github/GithubReducer.js
@@ -1,3 +1,5 @@
+import { act } from "react-dom/test-utils";
+
 const githubReducer = (state, action) => {
   switch (action.type) {
     case "GET_USERS":
@@ -16,6 +18,12 @@ const githubReducer = (state, action) => {
       return {
         ...state,
         users: []
+      }
+    case "GET_REPOS":
+      return {
+        ...state,
+        repos: action.payload,
+        loading: false
       }
     case "SET_LOADING":
       return {
